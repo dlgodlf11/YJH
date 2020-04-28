d0ece57bd3b91b4ffcce833a88cd0079a6201576
haeil <dlgodlf111@naver.com>
2020-04-28 18:59:33 +0900

    add anim

--- src/components/component-one.vue
+++ src/components/component-one.vue
@@ -6,7 +6,7 @@
     </v-row>
     <v-row align="center" justify="center">
       <transition name="puff">
-        <v-btn v-if="show" x-large dark @click="onclick">start</v-btn>
+        <v-btn class="jello-horizontal" v-if="show" x-large dark @click="onclick">start</v-btn>
       </transition>
       {{ showbtn }}
     </v-row>
@@ -81,6 +81,32 @@ export default {
     transform-origin: 50% 50%;
     text-shadow: 0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc,
       0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3);
+  }
+}
+.jello-horizontal {
+  animation: jello-horizontal 5s infinite both;
+}
+@keyframes jello-horizontal {
+  0% {
+    transform: scale3d(1, 1, 1);
+  }
+  5% {
+    transform: scale3d(1.25, 0.75, 1);
+  }
+  10% {
+    transform: scale3d(0.75, 1.25, 1);
+  }
+  15% {
+    transform: scale3d(1.15, 0.85, 1);
+  }
+  20% {
+    transform: scale3d(0.95, 1.05, 1);
+  }
+  25% {
+    transform: scale3d(1.05, 0.95, 1);
+  }
+  30% {
+    transform: scale3d(1, 1, 1);
   }
 }
 </style>
